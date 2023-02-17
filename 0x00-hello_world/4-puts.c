f1033123b3b75b64a2d22113d1ddce0c2831329c
Mibonacci <mohammadramdaoui@gmail.com>
2023-02-16 16:21:37 -0800

    taskfour

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Descirption: C program that prints with put
+ *
+ * Return: always 0 (working)
+ */
+
 int main(void)
-    {
-        puts("\"Programming is like building a multilingual puzzle");
-        return(0);
-    }
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
