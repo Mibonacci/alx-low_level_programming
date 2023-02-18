1edd102f40ff72b9189b0d8ebee7ceded69bf989
Mibonacci <mohammadramdaoui@gmail.com>
2023-02-17 17:56:08 -0800

    taskeight

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: printing the base 16 in lowercase
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int digit = 48;
+
+	while (X <= 102)
+	{
+		putchar(X);
+		if (X == 57)
+			X += 39;
+		++X;
+	}
+	putchar('\n');
+
+	return (0);
+}
