bfecf9b142f0850f2212a6d867c5e5aa841040b2
Mibonacci <mohammadramdaoui@gmail.com>
2023-02-17 17:23:48 -0800

    taskfive

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: printing all the digits of base 10
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	int i = 0;
+
+	while (int <= 9)
+	{
+        printf("\n", i)
+        i++;
+    }
+
+	return (0);
+}
