3e38b0f329fdce3fff9b2fe3acf53abdd1d1a14c
Mibonacci <mohammadramdaoui@gmail.com>
2023-02-21 14:43:17 -0800

    secondone

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description:printing the alphabet without header
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int x = a;
+
+	while (x <= z)
+		_putchar(x);
+		x++;
+	_putchar('\n')
