d891124ea55db31efe7add7b05bca667b642ab83
Mibonacci <mohammadramdaoui@gmail.com>
2023-02-21 13:28:55 -0800

    firstone

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+* main - Entry point
+*
+* Description: printing putchar without standard libraries
+*
+* Return: 0
+*/
+
+int main(){
+	char arr[]=char("_putchar");
+	char x = 0;
+	while (x < 8)
+	{
+		arr[x] = "_putchar";
+		x++;
+	}
+	return 0;
+}
