bd4bcc77b266a861d68e6a2d9c2049e9bf2f2f6d
Mibonacci <mohammadramdaoui@gmail.com>
2023-02-21 15:50:56 -0800

    thirdone

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- * print_alphabet - printing the alphabet without header
- * print_alphabet_x10 - printing the alphabet in 10 lines
+ * print_alphabet_x10 - printing the alphabet ten times
  *
-*/
+ */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-        int x, y;
+	int x, y;
 
 	for (y = 0; y <= 9; y++)
 	{
 		for (x = 'a'; x <= 'z'; x++)
-                _putchar(x);
-        _putchar('\n');
+		_putchar(x);
+	_putchar('\n');
 	}
 }
