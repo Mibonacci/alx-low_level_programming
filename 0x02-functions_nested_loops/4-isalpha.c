af9386ff39a8ef98ddeb4c3e60e0d0b7b4f575e8
Mibonacci <mohammadramdaoui@gmail.com>
2023-02-21 16:47:21 -0800

    finally

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - function
+ *
+ * @c: the parameter to check with
+ *
+ * Return: comes with 1 if a letter. otherwise 0
+ */
+
+int _isalpha(int c);
+{
+	if (c >= 65 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
