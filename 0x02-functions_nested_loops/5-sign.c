c8ce60573a67633b40ed11954a51ee1bb42a6fef
Mibonacci <mohammadramdaoui@gmail.com>
2023-02-21 17:31:49 -0800

    betty is getting good

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - function
+ *
+ * @n: the parameter to check with
+ *
+ * Return: returns 1 if positive and -1 if negative otherwise 0
+ */
+
+int print_sign(int n)
+	if (n < 0)
+	{
+		return (-1);
+		_putchar(45 + 0);
+	}
+	else if (n > 0)
+	{
+		return (1);
+		_putchar(0 + 43);
+	}
+	else
+	{
+		return (0);
+		_putchar(0 + 48);
+	}
+
