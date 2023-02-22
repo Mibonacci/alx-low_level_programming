610bee80e63eb1121442890dcb24e43701d1aac9
Mibonacci <mohammadramdaoui@gmail.com>
2023-02-21 18:12:05 -0800

    nice

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - easy one
+ * @a: is a
+ * @b: is b
+ * Return: what you have without the two points
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
