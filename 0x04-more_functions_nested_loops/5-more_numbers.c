7f849b11515bd46ee03cec5ac0d61d7db2dfae57
Mibonacci <mohammadramdaoui@gmail.com>
2023-02-23 15:48:32 -0800

    sixth

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,8 +10,12 @@ void more_numbers(void)
 
 	for (y = 0; y <= 9; y++)
 	{
-		for (x = '0'; x <= "14" ; x++)
-		_putchar(x);
+		for (x = 0; x <= 14; x++)
+		{
+			if (x >= 10)
+				_putchar('1');
+			_putchar (x % 10 + '0');
+		}
 	_putchar('\n');
 	}
 }
