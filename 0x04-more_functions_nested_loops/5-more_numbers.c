4652258b04b390cc3daee0c6cc2826f1b63a0ae1
Mibonacci <mohammadramdaoui@gmail.com>
2023-02-23 15:32:38 -0800

    fifth

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * more_numbers - print 10 times the numbers
+ *
+ */
+
+void more_numbers(void)
+{
+	int  x, y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		for (x = 48; x <= 49 52 ; x++)
+		_putchar(x);
+	_putchar('\n');
+	}
+}
