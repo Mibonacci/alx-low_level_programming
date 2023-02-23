66ee9fc63f4788af04af7f4db770126c250ffe38
Mibonacci <mohammadramdaoui@gmail.com>
2023-02-23 12:25:48 -0800

    hi

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check uppercase
+ * @c: operand
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
