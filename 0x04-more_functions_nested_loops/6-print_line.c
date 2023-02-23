0d2b0f51a510448c6babcb98538f754060d380ca
Mibonacci <mohammadramdaoui@gmail.com>
2023-02-23 15:56:30 -0800

    seventh

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line
+ *@n: tester
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	for (x = 0, x < n; x++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
