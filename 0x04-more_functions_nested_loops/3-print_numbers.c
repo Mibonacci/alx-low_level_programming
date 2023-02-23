d060036c3259c4aa576cf25b6e21ab643a3d67b9
Mibonacci <mohammadramdaoui@gmail.com>
2023-02-23 13:46:19 -0800

    fourth

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from zero to nine
+ * Return: 
+ */
+
+void print_numbers(void);
+{
+	int x;
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+	_putchar(\n);
+}
