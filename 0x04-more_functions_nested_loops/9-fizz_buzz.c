cba64916d98b8505bcd1b4a464b9ff8a50084d49
Mibonacci <mohammadramdaoui@gmail.com>
2023-02-23 16:40:22 -0800

    tenth

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+/**
+ * fizzbuzz - print fizz and buzz with conditions
+ * Return: zero
+ */
+
+int fizzbuzz(void)
+{
+	int n;
+
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
+		/**general condition*/
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+			/**the contion for fizz and buzz*/
+		{
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+			/**the contion for fizz */
+		{
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+			/**the contion for buzz*/
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
