b641ac801305a9e7cfc2d972e45cf7a58eae794d
Mibonacci <mohammadramdaoui@gmail.com>
2023-02-27 13:35:35 -0800

    task five

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string in reverse
+ * @s: the character
+ *
+ */
+
+void print_rev(char *s)
+{
+	strrev(s);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@ int _strlen(char *s);
 #include <stdio.h>
 #include <string.h>
 void _puts(char *str);
+void print_rev(char *s);
