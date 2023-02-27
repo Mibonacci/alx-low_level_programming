c02dba180e56530461d8ce78dcccf4025b282ea0
Mibonacci <mohammadramdaoui@gmail.com>
2023-02-27 13:03:33 -0800

    task two

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps values of 2 integers
+ * @a: parameter one
+ * @b: parameter two
+ * return: zero
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap;
+
+	swap = &a;
+	*a = &b;
+	*b = &swap;
+}
