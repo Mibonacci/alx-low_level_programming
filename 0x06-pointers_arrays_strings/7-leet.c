9f0e185b6710ccb783693c059861bb50a89f1a49
Mibonacci <mohammadramdaoui@gmail.com>
2023-03-02 15:23:14 -0800

    new workstation

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -31,14 +31,3 @@ char *leet(char *s)
 	}
 	return (s);
 }
-
-int main(void)
-{
-	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-	char *p;
-
-	p = leet(s);
-	printf("%s", p);
-	printf("%s", s);
-	return (0);
-}
