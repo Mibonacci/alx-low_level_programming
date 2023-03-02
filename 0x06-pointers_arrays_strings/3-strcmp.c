36e0205252b6a474e7ba95ee11695cbcde2169c4
Mibonacci <mohammadramdaoui@gmail.com>
2023-03-02 14:25:44 -0800

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,10 +12,10 @@ int _strcmp(char *s1, char *s2)
 	char *x = s1;
 	char *y = s2;
 
-	while (*x != '\0' && *y != '\0' && *x == *y)
+	for (*x != '\0' && *y != '\0' && *x == *y)
 	{
 		x++;
 		y++;
 	}
-	Return(*x - *y);
+	return(*x - *y);
 }
