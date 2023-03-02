340cb9f702808ce78258d3d841e92f0ea9dea9c1
Mibonacci <mohammadramdaoui@gmail.com>
2023-03-02 13:35:19 -0800

    comparing two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: the difference between the two strings
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	char *x = s1;
+	char *x = s2;
+
+	while (*x != '\0' && *y != '\0' && *x == *y)
+	{
+		x++;
+		y++;
+	}
+	Return(*x - *y);
+}
