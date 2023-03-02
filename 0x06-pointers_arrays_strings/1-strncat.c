c47b306f7340a67a7744c3b240c553096ffa2b57
Mibonacci <mohammadramdaoui@gmail.com>
2023-03-02 05:26:06 -0800

    limited bytes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,14 +10,12 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int x;
-	int y;
+	int x = 0;
+	int y = 0;
 
-	x = 0;
 	while (dest[x])
 		x++;
 	dest[x] = ' ';
-	y = 0;
 	while (src[y] && y < n)
 	{
 		dest[x] = src[y];
