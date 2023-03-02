264d70d627118ba599d46ad0b53979579d37d97d
Mibonacci <mohammadramdaoui@gmail.com>
2023-03-02 10:30:48 +0000

    concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @dest: first string
+ * @src: second string
+ * Return: first string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+    int x;
+    int y;
+
+    x = 0;
+    while (dest[x])
+    {
+        x++;
+    dest[x] = ' ';
+    }
+    y = 0;
+    while (src[y])
+    {
+        dest[x] = src[y];
+        y++;
+        x++;
+    }
+    dest = "\0";
+    return (dest);
+}
