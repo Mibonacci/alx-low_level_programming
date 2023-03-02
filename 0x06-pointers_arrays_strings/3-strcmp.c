7102a058c1aa06186d8ae1d728ef9bb996db865f
Mibonacci <mohammadramdaoui@gmail.com>
2023-03-02 15:00:53 -0800

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,13 @@ int _strcmp(char *s1, char *s2)
 	int x;
 
 	x = 0;
-	while (s1[x] == s2[x] && s1[x] != s2[x])
+	while (s1[x] == s2[x])
 	{
-		return (s1[x] - s2[x]);
+		if (s1[x] != s2[x])
+		{
+			return (s1[x] - s2[x]);
+		}
+		x++
 	}
-	x++;
+	return (0);
 }
