f20e06b70ad29e4c42f4927dce3059922cdcd676
Mibonacci <mohammadramdaoui@gmail.com>
2023-03-10 17:54:56 -0800

    hada lma39oul

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: prints the file name
+ * @argc: the number of arguments.
+ * @argv: the array of arguments.
+ * Return: always 0.
+ */
+
+int main(int argc, char const *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
